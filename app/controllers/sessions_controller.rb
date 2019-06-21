ca00a9e1bb086ea8b0405daf8bf4cd54172641c8
tarrinros <tarindis@gmail.com>
2019-06-21 20:31:32 +0300

    Set render resourse method to App Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,13 @@
 class SessionsController < Devise::SessionsController
   respond_to :json
 
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    render_resource(resource)
+  end
+
   private
 
   def respond_with(resource, _opts = {})
